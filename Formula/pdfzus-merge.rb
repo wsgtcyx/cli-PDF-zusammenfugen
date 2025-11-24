658b0149aebea60a12ab3d16bcaf4065bebaaec0
wsgtcyx <439248005@qq.com>
2025-11-24 22:27:05 +0800

    add formula

--- Formula/pdfzus-merge.rb
+++ Formula/pdfzus-merge.rb
@@ -3,8 +3,8 @@ require "language/node"
 class PdfzusMerge < Formula
   desc "Local command-line PDF merge tool built with pdf-lib"
   homepage "https://pdfzus.de"
-  url "https://github.com/andy/pdfzus/releases/download/v0.1.0/pdfzus-merge-0.1.0.tar.gz"
-  sha256 :no_check
+  url "https://github.com/wsgtcyx/cli-PDF-zusammenfugen/releases/download/v0.1.0/pdfzus-merge-0.1.0.tgz"
+  sha256 :"72f7491dd323d7bb51686d78a518dd2192acfdf59f0bd82dd99defccbe51345e"
   license "MIT"
 
   depends_on "node"
